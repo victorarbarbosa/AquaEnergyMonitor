bcec62c62d070ac6226b20539c7d3bd6109ba3a9
Victor Barbosa <victorabarbosa03@gmail.com>
2025-04-28 19:24:26 -0300

    adicionando ajustes na pagina home.

---
+++ AquaEnergyMonitor/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AquaEnergyMonitor.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
